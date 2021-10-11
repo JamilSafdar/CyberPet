8330764bbbc515210300d2914621d50d883597ca
Jamil Safdar <jamil.safdar@ao.com>
2021-10-11 15:13:55 +0100

    first commit

---
+++ cyberpet.js
@@ -0,0 +1,15 @@
+let input = prompt("input your details here:")
+console.log(input)
+
+
+class Animal{
+    constructor(name){
+        this._name = name;
+        this._event = event;
+        this._endOfPlay = endGame;
+        this._feed = feed;
+        this._play = play;
+        this._checkIn = checkOnAnimal
+
+    }
+}

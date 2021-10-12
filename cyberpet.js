bcffb30e9b56db7e0d18bf59b99044276b2e67fe
Jamil Safdar <jamil.safdar@ao.com>
2021-10-12 12:00:04 +0100

    created inherited classes

--- cyberpet.js
+++ cyberpet.js
@@ -1,17 +1,15 @@
 let input = prompt("input your details here:");
 console.log(input);
-
+// --global/generic variables--
+    // this._event = event;
+    // this._endOfGame = endGame;
+    // this._checkIn = checkOnAnimal;
+    // this._happinessLevel = 0;
+    // this._rest = rest;
+    // this._restLevel = 0;
 class Animal {
-  constructor(name) {
-    this._name = name;
-    this._event = event;
-    this._endOfGame = endGame;
-    this._feed = feed;
-    this._play = play;
-    this._checkIn = checkOnAnimal;
-    this._happinessLevel = 0;
-    this._rest = rest;
-    this._restLevel = 0;
+  constructor(petName) {
+    this._petName = petName;
   }
 
   get event() {
@@ -46,10 +44,49 @@ class Animal {
 }
 
 const showHappinessLevel = (feed, play) => {
-  for (let i = 0; i < happinessLevel; i++) {
+  for (let i = 0; i <= happinessLevel; i++) {
     feed += happinessLevel;
   }
-  for (let i = 0; i < happinessLevel; i++) {
+  for (let i = 0; i <= happinessLevel; i++) {
     play += happinessLevel;
   }
 };
+const showRestLevel = (rest, play) => {
+  for (let i = 0; i <= restLevel; i++) {
+    rest += restLevel;
+  }
+  for (let i = 0; i <= restLevel; i--) {
+    play -= restLevel;
+  }
+};
+class Dog extends Animal {
+  constructor(name, feed, play) {
+    super(name);
+    this.feed = feed;
+    this.play = play;
+  }
+  show() {
+    return 'You have fed ' + this.name + 'and made him this happy ' + this.showHappinessLevel();
+    return 'You have played with ' + this.name + 'and now ' + this.name + ' needs to rest ' + this.showRestLevel;
+  }
+}
+class Cat extends Animal {
+  constructor(name, feed, play) {
+    super(name);
+    this.feed = feed;
+    this.play = play;
+  }
+  show() {
+    return 'You have made ' + this.name + 'this happy ' + this.showHappinessLevel();
+  }
+}
+class Rabbit extends Animal {
+  constructor(name, feed, play) {
+    super(name);
+    this.feed = feed;
+    this.play = play;
+  }
+  show() {
+    return 'You have made ' + this.name + 'this happy ' + this.showHappinessLevel();
+  }
+}
